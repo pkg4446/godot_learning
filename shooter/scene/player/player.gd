739c24340c2a03646569173aac437913fb6ceb00
pkg4446 <pkg4446@gmail.com>
2023-09-27 15:53:16 +0900

    230927

--- shooter/scene/player/player.gd
+++ shooter/scene/player/player.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
-func _process(delta):
+func _process(_delta):
 	#input
 	var direction = Input.get_vector("left","right","up","down")
-	position += direction * 500 * delta
-
+	velocity = direction * 500
+	move_and_slide()
+	
 	# laser shooting input
 	if Input.is_action_pressed("primary_action"):
 		print("shooting")
